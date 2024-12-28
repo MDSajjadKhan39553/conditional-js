48e91c06b44b63f3d740fb3fe5e35bbfd133f40b
MD Sajjad Khan <mdsajjadkhan39553@gmail.com>
2024-12-28 19:11:01 +0600

    array

--- array.js
+++ array.js
@@ -7,16 +7,41 @@
 
 // array te add korarjonno
 
-const numbers = [23, 55, 45, 38, 47];
+// const numbers = [23, 55, 45, 38, 47];
 // numbers.push(88);
 // console.log(numbers)
 
 // array er ses element "pop" & first element ber korar jonno "shift" ber korar jonno
 // surute element dhukate "unshift" seser dike add korote "push"
 
-numbers.pop();
-console.log(numbers);
-numbers.shift();
-console.log(numbers);
-numbers.unshift(999);
-console.log(numbers);
+// numbers.pop();
+// console.log(numbers);
+// numbers.shift();
+// console.log(numbers);
+// numbers.unshift(999);
+// console.log(numbers);
+
+
+// includes er jonno
+
+// const name = ['salam', 'kalam', 'balam', 'malam']
+// if (name.includes('salam')) {
+//     console.log("you are salam boy");
+// } else {
+//     console.log("you are not salam");
+// }
+// // position ber korar jonno indexof use hoy 
+// console.log(name.indexOf('kalam'));
+
+// // kono kicho array kina check korar jonni "Array.isArray" use hoy
+// console.log(Array.isArray(name));
+
+// -----------------------array methods------------------------
+
+// 1. includes
+// 2. indexOf
+// 3. isArray
+// 4. join + or |
+// 5. concat
+// 6. slice
+// 7. splice
