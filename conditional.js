e2ab992783db6e4d8138f8e51be7900a7fed48c0
MD Sajjad Khan <mdsajjadkhan39553@gmail.com>
2024-12-28 18:30:17 +0600

    complete

---
+++ conditional.js
@@ -0,0 +1,97 @@
+// var isFoodReady = false
+
+// if (isFoodReady == true) {
+//     console.log("i will eat mama");
+// } else {
+//     console.log("i never eat mama");
+// }
+
+
+// var chickenPice = 180;
+// var myMoney = 850;
+
+// if (chickenPice < myMoney) {
+//     console.log("Yes! murgir Ran khamu ar haddi chabamu")
+// } else {
+//     console.log('Smashed potato with Lentil soup')
+// }
+
+// var iphonePrice = 70000;
+// var myBudget = 100000;
+
+// if (iphonePrice < myBudget) {
+//     console.log("mama iphone dia pic tule futani kormo")
+// } else {
+//     console.log ( "mama ami gorib futani bondho")
+// }
+
+// var burger = 499;
+
+// if (burger >= 500) {
+//     console.log("free drinks");
+// } else {
+//     console.log("drinks 30tk");
+// }
+// var bmi = 74/1.6^2;
+
+
+// if (bmi < 18.5) {
+//     console.log("you are underweight");
+// } else if (bmi >= 18.5 && bmi <= 24.9){
+//     console.log("you are normal");
+// }else if (bmi >= 25 && bmi <=29.9){
+//     console.log("you are overweight")
+// }
+// else {
+//     console.log("you are obese")
+// }
+
+
+// var result = 59;
+
+// if (result >= 90 && result <= 100){
+//     console.log("A");
+// }
+// else if(result >= 80 && result <= 89){
+//     console.log("B");
+// }
+// else if(result >= 70 && result <= 79){
+//     console.log("C");
+// }
+// else if(result >= 60 && result <= 69){
+//     console.log("D");
+// }
+// else{
+//     console.log("F");
+// }
+
+
+// var score = 80;
+
+// if (score > 80) {
+//     console.log("go for launch");
+// } 
+// else if(score < 80 || score >= 60){
+//     console.log("Good luck fot next time");
+// }
+// else if(score < 60 || score >= 40){
+//     console.log("keep your friend's message unseen");
+// }
+// else if(score < 40){
+//     console.log("block your friend");
+// }
+
+// else {
+    
+// }
+
+
+
+var num1 = 30;
+var num2 = 23;
+
+if (num1 > num2 ) {
+    result = num1+num2*2
+} else {
+    result = num1+num2
+}
