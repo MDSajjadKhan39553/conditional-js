f4742229b6a6c73bda26d08f1fc389368d72713d
MD Sajjad Khan <mdsajjadkhan39553@gmail.com>
2024-12-28 18:51:34 +0600

    array

---
+++ array.js
@@ -0,0 +1,22 @@
+// // array er lenth / koto gula ache seta dekhote
+// const number = [1, 3, 4, 5, 6];
+// console.log(number.length);
+
+// // arry er index ber kora
+// console.log(number[1]);
+
+// array te add korarjonno
+
+const numbers = [23, 55, 45, 38, 47];
+// numbers.push(88);
+// console.log(numbers)
+
+// array er ses element "pop" & first element ber korar jonno "shift" ber korar jonno
+// surute element dhukate "unshift" seser dike add korote "push"
+
+numbers.pop();
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+numbers.unshift(999);
+console.log(numbers);
