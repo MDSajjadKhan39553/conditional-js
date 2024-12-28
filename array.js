c1c5c1efb69c052e19416ed41829beed2d77c4c7
MD Sajjad Khan <mdsajjadkhan39553@gmail.com>
2024-12-28 19:35:30 +0600

    home-work

--- array.js
+++ array.js
@@ -1,40 +1,40 @@
-// // array er lenth / koto gula ache seta dekhote
-// const number = [1, 3, 4, 5, 6];
-// console.log(number.length);
+// array er lenth / koto gula ache seta dekhote
+const number = [1, 3, 4, 5, 6];
+console.log(number.length);
 
-// // arry er index ber kora
-// console.log(number[1]);
+// arry er index ber kora
+console.log(number[1]);
 
 // array te add korarjonno
 
-// const numbers = [23, 55, 45, 38, 47];
-// numbers.push(88);
-// console.log(numbers)
+const numbers = [23, 55, 45, 38, 47];
+numbers.push(88);
+console.log(numbers)
 
 // array er ses element "pop" & first element ber korar jonno "shift" ber korar jonno
 // surute element dhukate "unshift" seser dike add korote "push"
 
-// numbers.pop();
-// console.log(numbers);
-// numbers.shift();
-// console.log(numbers);
-// numbers.unshift(999);
-// console.log(numbers);
+numbers.pop();
+console.log(numbers);
+numbers.shift();
+console.log(numbers);
+numbers.unshift(999);
+console.log(numbers);
 
 
 // includes er jonno
 
-// const name = ['salam', 'kalam', 'balam', 'malam']
-// if (name.includes('salam')) {
-//     console.log("you are salam boy");
-// } else {
-//     console.log("you are not salam");
-// }
-// // position ber korar jonno indexof use hoy 
-// console.log(name.indexOf('kalam'));
+const name = ['salam', 'kalam', 'balam', 'malam']
+if (name.includes('salam')) {
+    console.log("you are salam boy");
+} else {
+    console.log("you are not salam");
+}
+// position ber korar jonno indexof use hoy 
+console.log(name.indexOf('kalam'));
 
-// // kono kicho array kina check korar jonni "Array.isArray" use hoy
-// console.log(Array.isArray(name));
+// kono kicho array kina check korar jonni "Array.isArray" use hoy
+console.log(Array.isArray(name));
 
 // -----------------------array methods------------------------
 
---
+++ home-work.js
@@ -0,0 +1,18 @@
+// const numbers = [12, 34, 53, 24, 24];
+// console.log(numbers[3]);
+// numbers[2] = "23"
+// console.log(numbers);
+// const destinations = ['dhaka', 'sylhet', 'cox'];
+// destinations.push('gazipur');
+// console.log(destinations);
+// destinations.push('rangpur', 'barishal');
+// console.log(destinations);
+// destinations.pop('barishal');
+// console.log(destinations);
+
+// const books = ['bangla', 'english', 'gonit'] 
+// console.log(Array.isArray('javascript'));
+
+const books = ['bangla', 'english', 'gonit'] 
+const destinations = ['dhaka', 'sylhet', 'cox'];
+console.log(books.concat(destinations));
